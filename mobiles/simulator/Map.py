a1d7c8a8194e37a95a612d9a32106f0422c94d3a
Li-Bingzi <LiBIngzi_99@icloud.com>
2020-08-23 15:26:34 +0800

    add skeleton of mobile simulation

---
+++ mobiles/simulator/Map.py
@@ -0,0 +1,10 @@
+from shapely import geometry
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import numpy as np
+
+
+class Map:
+    def __init__(self):
+        # define a Singapore map boundary
+        pass
